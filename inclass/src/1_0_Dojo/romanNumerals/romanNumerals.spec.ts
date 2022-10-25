2ad176fa32ddad2bb81d1ea9db5a6e0013cb73bf
César González <chesarglez429@gmail.com>
2022-10-24 21:05:25 -0500

    :white_check_mark: Convert arabic numbers to one-letter roman numbers

--- inclass/src/1_0_Dojo/romanNumerals/romanNumerals.spec.ts
+++ inclass/src/1_0_Dojo/romanNumerals/romanNumerals.spec.ts
@@ -1,7 +1,21 @@
 import { romanNumbers } from './romanNumerals'
 
+const generateRomanRelationship = (input: number, output: string) => ({ input, output })
+
 describe('Testing roman numerals conversion', () => {
   it('should return empty string if 0', () => {
     expect(romanNumbers(0)).toBe('')
+  })
+
+  it.each([
+    generateRomanRelationship(1, 'I'),
+    generateRomanRelationship(5, 'V'),
+    generateRomanRelationship(10, 'X'),
+    generateRomanRelationship(50, 'L'),
+    generateRomanRelationship(100, 'C'),
+    generateRomanRelationship(500, 'D'),
+    generateRomanRelationship(1000, 'M'),
+  ])('If the user enters $input, we expect the output to be $output', ({ input, output }) => {
+    expect(romanNumbers(input)).toBe(output)
   })
 })
--- inclass/src/1_0_Dojo/romanNumerals/romanNumerals.ts
+++ inclass/src/1_0_Dojo/romanNumerals/romanNumerals.ts
@@ -1,3 +1,19 @@
+const ROMAN_TO_ARABIC_UNITS = [
+  {roman: 'I', arabic: 1},
+  {roman: 'V', arabic: 5},
+  {roman: 'X', arabic: 10},
+  {roman: 'L', arabic: 50},
+  {roman: 'C', arabic: 100},
+  {roman: 'D', arabic: 500},
+  {roman: 'M', arabic: 1000},
+]
+
 export const romanNumbers = (num: number) => {
-  return ''
+  if (num === 0) {
+    return '';
+  }
+  
+  const { roman } = ROMAN_TO_ARABIC_UNITS.find(({ arabic }) => arabic === num);
+
+  return roman;
 }
